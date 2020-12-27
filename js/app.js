6cd893dc43cb85065b266d2128bb270360f7981a
Abbas Salami <olawalesalami3@gmail.com>
2020-12-27 11:43:30 +0100

    dashboard

--- js/app.js
+++ js/app.js
@@ -6,3 +6,64 @@ $(".navbar ul a#abt").on("click", function (e) {
     // 3
     $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
 });
+
+// Dashboard
+/* globals Chart:false, feather:false */
+
+(function () {
+    'use strict'
+  
+    feather.replace()
+  
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday'
+        ],
+        datasets: [{
+          data: [
+            10,
+            5,
+            7,
+            11,
+            3,
+            1,
+            8
+          ],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        title: {
+            display: 'true',
+            text: 'Events distribution chart',
+            padding: 5
+        }
+      }
+    })
+  })()
+  
